d0a9c651b3e2818cebf99432137c7acc2a32c011
lusikawurtz <lusika.z777@gmail.com>
2024-07-31 18:54:49 +0400

    git init

---
+++ src/main/java/com/nc1_test/Nc1TestApplication.java
@@ -0,0 +1,13 @@
+package com.nc1_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nc1TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nc1TestApplication.class, args);
+    }
+
+}
